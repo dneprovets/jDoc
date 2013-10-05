1a051bab88a00fb29a331fc8644e504f8987d4ef
webschik <webschik@gmail.com>
2013-10-05 20:57:01 +0300

    Change Grunt tasks structure. Add tasks: readers, writers, readers:include, readers:exclude, writers:include, writers:exclude

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,103 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jdoc-engines-concat');
     grunt.loadNpmTasks('grunt-jsdoc');
 
+    var enginesConcatTaskName = "engines_concat",
+        /**
+         *
+         * @param list {Array}
+         * @param type {"reader"|"writer"}
+         */
+        concatEngines = function (list, type, isExclude) {
+            list = list || [];
+
+            isExclude = (isExclude == null) ? true : isExclude;
+
+            var len = list.length,
+                engineName,
+                isNeedInclude,
+                tasksList = [],
+                enginesConcatTaskList = grunt.config.data[enginesConcatTaskName];
+
+            for (engineName in enginesConcatTaskList) {
+                isNeedInclude = false;
+                if (isExclude) {
+                    if (len && list.indexOf(engineName) < 0) {
+                        isNeedInclude = true;
+                    }
+                } else {
+                    if (!len || list.indexOf(engineName) >= 0) {
+                        isNeedInclude = true;
+                    }
+                }
+
+                if (
+                    enginesConcatTaskList.hasOwnProperty(engineName) && isNeedInclude &&
+                        enginesConcatTaskList[engineName][type]
+                ) {
+                    tasksList.push(enginesConcatTaskName + ":" + engineName + ":" + type);
+                }
+            }
+
+            tasksList = tasksList.concat([
+                'concat:app',
+                'clean:engines',
+                'uglify'
+            ]);
+
+            grunt.task.run(tasksList);
+        },
+        /**
+         * @param type {"reader"|"writer"}
+         * @returns {Array}
+         */
+        getEnginesList = function (type) {
+            var flags = grunt.option.flags().join('').replace(/[=]/g, '').split('--'),
+                i,
+                flag,
+                enginesConcatTaskList = grunt.config.data[enginesConcatTaskName] || {},
+                engines = [],
+                len = flags.length;
+
+            console.log(flags);
+
+            for (i = len - 1; i >= 0; i--) {
+                flag = flags[i].replace(/^\-*/, '').toLowerCase();
+
+                if (enginesConcatTaskList[flag] && enginesConcatTaskList[flag][type]) {
+                    engines.push(flag);
+                }
+            }
+
+            return engines;
+        };
+
+    grunt.registerTask('readers', function () {
+        var type = "reader";
+        concatEngines(getEnginesList(type), type, false);
+    });
+    grunt.registerTask('readers:include', function () {
+        var type = "reader";
+        concatEngines(getEnginesList(type), type, false);
+    });
+    grunt.registerTask('readers:exclude', function () {
+        var type = "reader";
+        concatEngines(getEnginesList(type), type, true);
+    });
+    grunt.registerTask('writers', function () {
+        var type = "writer";
+        concatEngines(getEnginesList(type), type, false);
+    });
+    grunt.registerTask('writers:include', function () {
+        var type = "writer";
+        concatEngines(getEnginesList(type), type, false);
+    });
+    grunt.registerTask('writers:exclude', function () {
+        var type = "writer";
+        concatEngines(getEnginesList(type), type, true);
+    });
     grunt.registerTask('default', [
-        'engines_concat',
-        'concat:app',
-        'clean:engines',
-        'uglify'/*,
-        'jsdoc'*/
+        'readers',
+        'writers'/*,
+         'jsdoc'*/
     ]);
 };
