d91dac1de36b52087e40a6f9da4a76d28be48fb1
webschik <webschik@gmail.com>
2014-08-20 23:20:11 +0300

    Add break lines support to Simple engine (txt, etc)

--- src/build/jdoc.0.5.1.js
+++ src/build/jdoc.0.5.1.js
@@ -16702,39 +16702,58 @@
             createFileData: function (text, callback) {
                 var textContent,
                     i,
+                    j,
+                    l,
                     len,
+                    textLines,
                     textSections,
                     breaks,
                     element,
                     isLink = false,
                     children = [];
 
-                textSections = text.split(/\s/);
-                len = textSections.length;
+                textLines = text.split(/\n/);
+                l = textLines.length;
 
-                for (i = 0; i < len; i++) {
-                    if (textSections[i]) {
-                        isLink = jDoc.validateURL(textSections[i]);
+                for (j = 0; j < l; j++) {
+                    textSections = textLines[j].split(/\s/);
+                    len = textSections.length;
 
-                        element = {
-                            options: {},
+                    for (i = 0; i < len; i++) {
+                        if (textSections[i]) {
+                            isLink = jDoc.validateURL(textSections[i]);
+
+                            element = {
+                                options: {},
+                                css: {},
+                                attributes: {},
+                                properties: {
+                                    textContent: textSections[i] + " "
+                                }
+                            };
+
+                            if (isLink) {
+                                breaks = textSections[i].replace(/\S+/g, '') + " ";
+                                textContent = textSections[i].replace(/\s+/, '');
+                                element.isLink = true;
+                                element.href = textContent;
+                                element.properties.textContent = textContent;
+                                element.after = breaks;
+                            }
+
+                            children.push(element);
+                        }
+                    }
+
+                    if (j < l - 1) {
+                        children.push(element = {
+                            options: {
+                                isEmptyLine: true
+                            },
                             css: {},
                             attributes: {},
-                            properties: {
-                                textContent: textSections[i] + " "
-                            }
-                        };
-
-                        if (isLink) {
-                            breaks = textSections[i].replace(/\S+/g, '') + " ";
-                            textContent = textSections[i].replace(/\s+/, '');
-                            element.isLink = true;
-                            element.href = textContent;
-                            element.properties.textContent = textContent;
-                            element.after = breaks;
-                        }
-
-                        children.push(element);
+                            properties: {}
+                        });
                     }
                 }
 
--- tests/testDocsList.js
+++ tests/testDocsList.js
@@ -4,5 +4,6 @@ window['testDocsList'] = [
     "http://localhost:9000/tests/docs/odf/UOML Sample.odt",
     "http://localhost:9000/tests/docs/ooxml/NET_-_ASP_NET_questions_with_references.docx",
     "http://localhost:9000/tests/docs/ooxml/hello world.docx",
-    "http://localhost:9000/tests/docs/rtf/sample.rtf"
+    "http://localhost:9000/tests/docs/rtf/sample.rtf",
+    "http://localhost:9000/tests/docs/simple/lorem.txt"
 ];
